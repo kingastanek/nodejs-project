d91f4494014d9e5b59e457ee574e195c22f190c3
Kinga <kin.stanek@gmail.com>
2020-05-16 21:50:29 +0200

    Editing the Product Data

--- controllers/admin.js
+++ controllers/admin.js
@@ -10,7 +10,7 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
   const { title, imageUrl, description, price } = req.body;
-  const product = new Product(title, imageUrl, description, price);
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 };
@@ -30,8 +30,23 @@ exports.getEditProduct = (req, res, next) => {
   });
 };
 
-exports.postEditProduct = () => {
-  
+exports.postEditProduct = (req, res, next) => {
+  const {
+    productId,
+    title: updatedTitle,
+    price: updatedPrice,
+    imageUrl: updatedImageUrl,
+    description: updatedDescription
+  } = req.body;
+  const updatedProduct = new Product(
+    productId,
+    updatedTitle,
+    updatedImageUrl,
+    updatedDescription,
+    updatedPrice,
+  );
+  updatedProduct.save();
+  res.redirect('/admin/products');
 }
 
 exports.getProducts = (req, res, next) =>{
