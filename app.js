fea31219f516de54a12c342c86249e190ef447f1
Kinga <kin.stanek@gmail.com>
2020-06-07 11:51:21 +0200

    Retrieving Data

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ const shopRoutes = require('./routes/shop');
 const pageNotFoundController = require('./controllers/404');
 const db = require('./util/database');
 
+db.execute('SELECT * FROM products')
+  .then((result) => console.log(result))
+  .catch(err => console.log(err));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
