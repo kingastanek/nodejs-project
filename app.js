c289b1c1f4c462dfc21ab6d76010cb6dc2d8619a
Kinga <kin.stanek@gmail.com>
2020-06-07 12:13:18 +0200

    Fetching Data

--- app.js
+++ app.js
@@ -11,11 +11,6 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const pageNotFoundController = require('./controllers/404');
-const db = require('./util/database');
-
-db.execute('SELECT * FROM products')
-  .then((result) => console.log(result))
-  .catch(err => console.log(err));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require('../models/product');
-const Cart =require('../models/cart');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -23,13 +23,15 @@ exports.getProduct = (req, res, next) => {
 }
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/index', {
-      prods: products,
-      pageTitle: 'Shop',
-      path: '/',
-    });
-  });
+  Product.fetchAll()
+    .then(([rows, fieldData]) => {
+      res.render('shop/index', {
+        prods: rows,
+        pageTitle: 'Shop',
+        path: '/',
+      });
+    })
+    .catch(err => console.log(err));
 }
 
 exports.getCart = (req, res, next) => {
