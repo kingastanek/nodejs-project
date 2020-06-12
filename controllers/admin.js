ca5db9d85faa43f60c742f307d12352bc0fd638f
Kinga <kin.stanek@gmail.com>
2020-06-12 18:07:00 +0200

    Inserting Data & Creating a Product

--- controllers/admin.js
+++ controllers/admin.js
@@ -10,9 +10,15 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
   const { title, imageUrl, description, price } = req.body;
-  const product = new Product(null, title, imageUrl, description, price);
-  product.save().then(() => res.redirect('/')).catch(err => console.log(err));
-  res.redirect('/');
+  Product
+    .create({
+      title,
+      imageUrl,
+      price,
+      description
+    })
+    .then(result => console.log(result))
+    .catch(err => console.log(err));
 };
 
 exports.getEditProduct = (req, res, next) => {
