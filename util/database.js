68213c779ba03324e050bf038ec3609cf4a8cd92
Kinga <kin.stanek@gmail.com>
2020-06-12 17:52:12 +0200

    Syncing JS Definitions to the Database

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('new-schema', 'root', 'mysql', {
+const sequelize = new Sequelize('new_schema', 'root', 'mysql', {
   dialect: 'mysql',
   host: 'localhost',
 });
