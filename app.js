c789d9702c75c12a80fb6de71b9b55c160a54557
Kinga <kin.stanek@gmail.com>
2020-06-24 23:17:19 +0200

    Creating & Managing a Dummy User

--- app.js
+++ app.js
@@ -17,6 +17,12 @@ const pageNotFoundController = require('./controllers/404');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res, next) => {
+  User.findByPk(1).then(user => {
+    req.user = user;
+    next();
+  }).catch(err => console.log(err))
+})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -27,7 +33,18 @@ Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
 
 sequelize
-  .sync({ force: true })
-  .then(result => app.listen(3000))
+  // .sync({ force: true })
+  .sync()
+  .then(result => {
+    return User.findByPk(1)
+  })
+  .then(user => {
+    if (!user) return User.create({ name: 'Max', email: 'test@email.com'});
+    return user;
+  })
+  .then(user => {
+    console.log(user);
+    app.listen(3000)
+  })
   .catch(err => console.log(err));
 
